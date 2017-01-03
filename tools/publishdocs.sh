8b891e59eb3927bfea3b1471a2e7ee7be06857ad
Merijn Schering <mschering@intermesh.nl>
2017-01-03 12:02:46 +0100

    Ignore vendor folders

--- tools/publishdocs.sh
+++ tools/publishdocs.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 #apigen generate --source "../lib" --destination "../docs/apigen" --title "GroupOffice API" --charset "UTF-8" --exclude "*/vendor/*" --access-levels "public,protected" --internal "no" --php "yes" --tree "yes" --deprecated "no" --todo "no" --download "no" --source-code "yes" --colors "yes"
 
-apigen generate --debug --source ../lib/IFW --source ../GO --destination "../docs/apigen" --title "GroupOffice API" --exclude "*/vendor/*"
+apigen generate --debug --source ../lib/IFW --source ../GO --destination "../docs/apigen" --title "GroupOffice API" --exclude "*/vendor/*" --no-source-code
 
 #rm -Rf .tmp
 #mkdir -p .tmp/docs
