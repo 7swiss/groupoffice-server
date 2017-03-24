7bbe342a5233b50def1c215d6cea7c0a241b2435
root <root@amadeiro.intermesh.nl>
2017-03-24 10:58:38 +0100

    update query

--- GO/Modules/GroupOffice/Messages/Install/Database/20170324-1055.sql
+++ GO/Modules/GroupOffice/Messages/Install/Database/20170324-1055.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `messages_thread` ADD `modifiedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `id`;
-ALTER TABLE `messages_thread` CHANGE `modifiedAt` `modifiedAt` DATETIME NOT NULL;
+ALTER TABLE `messages_thread` ADD `modifiedAt` DATETIME NULL DEFAULT null AFTER `id`;
 update messages_thread set modifiedAt = lastMessageSentAt;
+ALTER TABLE `messages_thread` CHANGE `modifiedAt` `modifiedAt` DATETIME NOT NULL;
 
 ALTER TABLE `messages_message` ADD INDEX(`modifiedAt`);
 ALTER TABLE `messages_thread` ADD INDEX(`modifiedAt`);
