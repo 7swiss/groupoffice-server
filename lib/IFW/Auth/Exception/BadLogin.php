e740345939185306a3217582b56cd83869bf23c3
Merijn Schering <mschering@intermesh.nl>
2017-03-30 14:18:40 +0200

    Login by token

--- lib/IFW/Auth/Exception/BadLogin.php
+++ lib/IFW/Auth/Exception/BadLogin.php
@@ -5,7 +5,7 @@ use Exception;
 
 class BadLogin extends Exception {
 	
-	public function __construct($message = "Wron username or password", $code = 0, Exception $previous = null) {
+	public function __construct($message = "Wrong username or password", $code = 0, Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
 	
