df27faaeff9cde8a572f3c334da4654277503f7e
Merijn Schering <mschering@intermesh.nl>
2017-04-07 09:32:15 +0200

    Disable only_full_group_by

--- lib/IFW/Db/PDO.php
+++ lib/IFW/Db/PDO.php
@@ -14,7 +14,7 @@ namespace IFW\Db;
  */
 class PDO extends \PDO{
 	
-	private $sqlMode = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";
+	protected $sqlMode = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";
 	
 	public function __construct($dsn, $username, $passwd, $options=null) {
 		parent::__construct($dsn, $username, $passwd, $options);
