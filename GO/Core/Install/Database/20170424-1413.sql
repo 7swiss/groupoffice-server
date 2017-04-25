b6ba44a43ea1a4c813e2083f1a457abe487092f3
Merijn Schering <mschering@intermesh.nl>
2017-04-25 12:18:37 +0200

    Improved account management

--- GO/Core/Install/Database/20170424-1413.sql
+++ GO/Core/Install/Database/20170424-1413.sql
@@ -4,3 +4,6 @@ ALTER TABLE `smtp_account` CHANGE `port` `port` INT(11) NOT NULL DEFAULT '587';
 insert into accounts_capability select null,id, "GO\\Core\\Email\\Model\\Message" from accounts_account where modelName="GO\\Core\\Smtp\\Model\\Account";
 
 ALTER TABLE `accounts_account` ADD `deleted` BOOLEAN NOT NULL DEFAULT FALSE AFTER `ownedBy`;
+
+ALTER TABLE `smtp_account` CHANGE `password` `password` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
+
