ede98f58954b4f17a19020f24819f8110a762e6b
Merijn Schering <mschering@intermesh.nl>
2017-05-04 14:04:06 +0200

    clearstatcache()

--- lib/IFW/Fs/Folder.php
+++ lib/IFW/Fs/Folder.php
@@ -302,6 +302,7 @@ class Folder extends FileSystemObject {
 
 		try{
 			if (mkdir($this->path, $permissionsMode, true)) {
+				//needed for concurrency problems
 				clearstatcache();
 				if (isset($this->changeGroup)) {
 					chgrp($this->path, $this->changeGroup);
