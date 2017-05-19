c925f3298e7c838b8ea427250abf426f687f488f
dehart <derinus@gmail.com>
2017-05-19 11:13:44 +0200

    got duplicate key error in upgrade script so i removed the ID from the insert. This is already auto incremented

--- GO/Core/Install/Database/20170518-1600.sql
+++ GO/Core/Install/Database/20170518-1600.sql
@@ -1,8 +1,8 @@
 ALTER TABLE `auth_token` ADD `userAgent` VARCHAR(190) NULL AFTER `expiresAt`, ADD `remoteIpAddress` VARCHAR(50) NULL AFTER `userAgent`;
 
 
-INSERT INTO `cron_job` (`id`, `deleted`, `moduleId`, `name`, `cronClassName`, `method`, `params`, `cronExpression`, `enabled`, `runUserId`, `nextRun`, `lastRun`, `runningSince`, `timezone`, `lastError`) VALUES
-(4, 0, NULL, 'Garbage Collector', 'GO\\Core\\GarbageCollection\\Collector', 'collect', NULL, '0 0 * * *', 1, 1, '2017-05-20 00:00:00', NULL, NULL, 'UTC', NULL);
+INSERT INTO `cron_job` (`deleted`, `moduleId`, `name`, `cronClassName`, `method`, `params`, `cronExpression`, `enabled`, `runUserId`, `nextRun`, `lastRun`, `runningSince`, `timezone`, `lastError`) VALUES
+(0, NULL, 'Garbage Collector', 'GO\\Core\\GarbageCollection\\Collector', 'collect', NULL, '0 0 * * *', 1, 1, '2017-05-20 00:00:00', NULL, NULL, 'UTC', NULL);
 
 ALTER TABLE `notifications_watch` ADD FOREIGN KEY (`groupId`) REFERENCES `auth_group`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT; ALTER TABLE `notifications_watch` ADD FOREIGN KEY (`recordTypeId`) REFERENCES `orm_record_type`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
