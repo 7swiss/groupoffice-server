0e3e3b9f78ac1c19249852fce0c46bd07892b506
Merijn Schering <mschering@intermesh.nl>
2017-06-06 11:12:50 +0200

    Full name handling

--- GO/Modules/GroupOffice/Contacts/Install/Database/20170606-1046.sql
+++ GO/Modules/GroupOffice/Contacts/Install/Database/20170606-1046.sql
@@ -4,3 +4,5 @@ ALTER TABLE `contacts_contact` CHANGE `firstName` `firstName` VARCHAR(100) CHARA
 ALTER TABLE `contacts_contact` CHANGE `middleName` `middleName` VARCHAR(55) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `contacts_contact` CHANGE `lastName` `lastName` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL;
 ALTER TABLE `contacts_contact` CHANGE `suffixes` `suffixes` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'Suffixes like \'Msc.\'';
+ALTER TABLE `contacts_contact` CHANGE `prefixes` `prefixes` VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT 'Prefixes like ''Sir''';
+
