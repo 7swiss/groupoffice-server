a5858db4304bbbee869759fb031ca09ec2a8b355
Merijn Schering <mschering@intermesh.nl>
2017-06-12 09:28:30 +0200

    Don't flush cache on system check

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
+
+
 //Include the autoloader that composer has generated.
 //We need the classLoader variable so we can find objects in code later.
 $classLoader = require(dirname(__DIR__)."/vendor/autoload.php");
 
 use GO\Core\Web\App;
+
 
 //Create the app with the config.php file
 $app = new App($classLoader, require(dirname(__DIR__).'/config.php'));
