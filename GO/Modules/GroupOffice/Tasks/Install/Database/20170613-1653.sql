118553351cf2ba3f77166633f5bc85a4f4f846ac
Merijn Schering <mschering@intermesh.nl>
2017-06-16 08:14:36 +0200

    Tasks assignedTo is now a user group

--- GO/Modules/GroupOffice/Tasks/Install/Database/20170613-1653.sql
+++ GO/Modules/GroupOffice/Tasks/Install/Database/20170613-1653.sql
@@ -1,9 +1,10 @@
+ALTER TABLE tasks_task DROP FOREIGN KEY tasks_task_ibfk_1;
 ALTER TABLE `tasks_task` CHANGE `assignedTo` `assignedToBak` INT(11) NULL DEFAULT NULL;
 
 ALTER TABLE `tasks_task` ADD `assignedTo` INT NULL DEFAULT NULL AFTER `createdBy`, ADD INDEX (`assignedTo`);
 update tasks_task set assignedTo = (select id from auth_group where userId=assignedToBak);
 ALTER TABLE `tasks_task` ADD FOREIGN KEY (`assignedTo`) REFERENCES `auth_group`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE tasks_task DROP FOREIGN KEY tasks_task_ibfk_1;
+
 ALTER TABLE `tasks_task` DROP `assignedToBak`;
 
 
